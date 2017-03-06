8210281835136d663f9b600297e2bca3932a9066
Wassim Beltaief <beltaief.wassim@gmail.com>
2017-03-06 17:13:33 +0100

    add getMovies with retrofit and save movies

--- app/src/main/java/com/wassim/mvvmboilerplate/data/local/DatabaseHelper.java
+++ app/src/main/java/com/wassim/mvvmboilerplate/data/local/DatabaseHelper.java
@@ -1,11 +1,18 @@
 package com.wassim.mvvmboilerplate.data.local;
 
+import android.database.sqlite.SQLiteDatabase;
+
 import com.squareup.sqlbrite.BriteDatabase;
 import com.squareup.sqlbrite.SqlBrite;
+import com.wassim.mvvmboilerplate.MovieModel;
+import com.wassim.mvvmboilerplate.data.model.Movie;
+
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import rx.Observable;
 import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
@@ -33,5 +40,31 @@ public class DatabaseHelper {
 
     BriteDatabase getDb() {
         return mDb;
+    }
+
+    @SuppressWarnings("deprecation")
+    Observable<Movie> setMovies(List<Movie> movies) {
+        return Observable.create(subscriber -> {
+            if (subscriber.isUnsubscribed()) return;
+            BriteDatabase.Transaction transaction = mDb.newTransaction();
+            try {
+                for (Movie movie : movies) {
+                    long result = mDb.insert(
+                            MovieModel.TABLE_NAME,
+                            Movie.FACTORY.marshal(movie).asContentValues(),
+                            SQLiteDatabase.CONFLICT_REPLACE);
+                    if (result >= 0) subscriber.onNext(movie);
+                }
+                transaction.markSuccessful();
+                subscriber.onCompleted();
+            } finally {
+                transaction.end();
+            }
+        });
+    }
+
+    Observable<List<Movie>> getMovies() {
+        return mDb.createQuery(Movie.TABLE_NAME, Movie.SELECT_ALL)
+                .mapToList(Movie.MAPPER_ALL::map);
     }
 }
--- app/src/main/java/com/wassim/mvvmboilerplate/data/model/Movie.java
+++ app/src/main/java/com/wassim/mvvmboilerplate/data/model/Movie.java
@@ -5,6 +5,7 @@ import android.os.Parcelable;
 import com.google.auto.value.AutoValue;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
+import com.squareup.sqldelight.RowMapper;
 import com.wassim.mvvmboilerplate.MovieModel;
 import com.wassim.mvvmboilerplate.ui.base.MvvmModel;
 
@@ -14,6 +15,13 @@ import com.wassim.mvvmboilerplate.ui.base.MvvmModel;
 
 @AutoValue
 public abstract class Movie extends MvvmModel implements MovieModel, Parcelable {
+
+    public static final Factory<Movie> FACTORY = new Factory<>(AutoValue_Movie::new);
+
+    public static final RowMapper<Movie> MAPPER_ALL = FACTORY.select_allMapper();
+    public static final RowMapper<Movie> MAPPER_BY_ID = FACTORY.select_by_idMapper();
+    public static final RowMapper<Movie> TITLE_MAPPER = FACTORY.select_by_titleMapper();
+    public static final RowMapper<Movie> YEAR_MAPPER = FACTORY.select_by_release_yearMapper();
 
     public static TypeAdapter<Movie> typeAdapter(Gson gson) {
         return new AutoValue_Movie.GsonTypeAdapter(gson);
