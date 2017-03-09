33b578f5db8e280bfaaeae7ec9ed59b55ccc1070
WassimBeltaief <beltaief.wassim@gmail.com>
2017-03-09 15:06:21 +0100

    add viewModels tests

--- app/src/test/java/com/wassim/mvvmboilerplate/tests/views/main/MovieViewModelTest.java
+++ app/src/test/java/com/wassim/mvvmboilerplate/tests/views/main/MovieViewModelTest.java
@@ -1,11 +1,15 @@
 package com.wassim.mvvmboilerplate.tests.views.main;
 
+import android.view.View;
+
 import com.wassim.mvvmboilerplate.data.local.DataManager;
 import com.wassim.mvvmboilerplate.data.model.Movie;
 import com.wassim.mvvmboilerplate.ui.views.main.MovieViewModel;
 import com.wassim.mvvmboilerplate.util.RxSchedulersOverrideRule;
 import com.wassim.mvvmboilerplate.util.TestDataFactory;
+import com.wassim.mvvmboilerplate.util.Vars;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,9 +17,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Collections;
 import java.util.List;
 
 import rx.Observable;
+import rx.observers.TestSubscriber;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -54,4 +60,38 @@ public class MovieViewModelTest {
 
     }
 
+    @Test
+    public void onModelsLoaded() throws Exception {
+        List<Movie> movies = TestDataFactory.makeListMovie(2);
+
+        TestSubscriber<Integer> result = new TestSubscriber<>();
+        movieViewModel.viewModelSubject.subscribe(result);
+
+        movieViewModel.onModelsLoaded(movies);
+
+        Assert.assertEquals(View.GONE, movieViewModel.progressVisibility.get());
+        Assert.assertEquals(View.GONE, movieViewModel.statusVisibility.get());
+        Assert.assertEquals(View.VISIBLE, movieViewModel.recyclerVisibility.get());
+
+        result.assertValue(Vars.LOADED);
+        result.assertNotCompleted();
+
+    }
+
+    @Test
+    public void onModelsLoadedEmpty() throws Exception {
+
+        TestSubscriber<Integer> result = new TestSubscriber<>();
+        movieViewModel.viewModelSubject.subscribe(result);
+
+        movieViewModel.onModelsLoaded(Collections.emptyList());
+
+        Assert.assertEquals(View.GONE, movieViewModel.progressVisibility.get());
+        Assert.assertEquals(View.VISIBLE, movieViewModel.statusVisibility.get());
+        Assert.assertEquals(View.GONE, movieViewModel.recyclerVisibility.get());
+
+        result.assertNoValues();
+        result.assertNotCompleted();
+
+    }
 }
